b4cdd4c535f9c5299c1c8ecc6d03b6412326c981
markus-xavier <argitavis@gmail.com>
2021-02-13 18:18:14 -0600

    adds a command that sends a message to all players

--- annouce/annouce_client.js
+++ annouce/annouce_client.js
@@ -1,3 +1,10 @@
-RegisterCommand('annouce', (source, args) => {
-    emitNet('annouce', args.join(' '))
+RegisterCommand('annouce', (source, args) => { 
+    emitNet('annouce', args.join(' '));
+})
+
+RegisterNetEvent('no-perms');
+
+on('no-perms', () =>{
+    console.log(source);
+    emit('chat:addMessage', '[Error]', [255, 0, 0], 'You don\'t have permission to do that');
 })
--- annouce/annouce_server.js
+++ annouce/annouce_server.js
@@ -1,5 +1,12 @@
 RegisterNetEvent('annouce');
+
 on('annouce', (param) => {
-    console.log('^7[^1Annoucment^7]^2' + param);
-    emitNet('chatMessage', -1, '^7[^1Annoucment^7]^2', [0, 0, 0], param);
+    console.log(GetPlayerIdentifier(source, 0));
+    console.log(source);
+    if(IsPlayerAceAllowed(steamid, 'administrator')){
+        console.log('^7[^1Annoucment^7]^2' + param);
+    emitNet('chat:addMessage', -1, '^7[^1Annoucment^7]^2', [0, 0, 0], param);
+    } else {
+        emitNet('no-perms', source);
+    }
 })
